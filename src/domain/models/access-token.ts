c827a03594f6b23f8019820cee66e3d01bda715c
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-03-03 21:04:00 -0300

    ensure AccessToken expires in 30 minutess

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,8 @@
 export class AccessToken {
   constructor (private readonly value: string) {}
+
+  static get expirationInMs (): number {
+    // convertendo 30 minutos para 1800000 ms
+    return 30 * 60 * 1000
+  }
 }

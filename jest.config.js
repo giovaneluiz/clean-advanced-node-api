b6b5af6a8a52b3ca616bf3d2ed9e90998c701848
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-03-06 11:00:37 -0300

    remove folders test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/index.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   moduleNameMapper: {

b7897d16ef6f3d005664107407ed4287e6bd6b4b
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-03-06 13:52:18 -0300

    improve code quality

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -12,6 +12,11 @@ describe('Facebook Api', () => {
     httpClient.get
       .mockResolvedValueOnce({ access_token: 'any_app_token' })
       .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({
+        id: 'any_fb_id',
+        name: 'any_fb_name',
+        email: 'any_fb_email'
+      })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -55,6 +60,16 @@ describe('Facebook Api', () => {
         fields: 'id,name,email',
         access_token: 'any_client_token'
       }
+    })
+  })
+
+  it('should return facebook user', async () => {
+    const fbUser = await sut.loadUser({ token: 'any_client_token' })
+
+    expect(fbUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
     })
   })
 })
